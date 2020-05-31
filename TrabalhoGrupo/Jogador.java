c0d28d25591771bc0eb82bff0ebff0dbec51c2b1
Hugo <hugof220@gmail.com>
2020-05-31 20:21:14 +0100

    Classe Jogador completa
    Classe concluída

--- TrabalhoGrupo/Jogador.java
+++ TrabalhoGrupo/Jogador.java
@@ -2,66 +2,77 @@ package TrabalhoGrupo;
 
 import java.util.Date;
 
-public class Jogador extends Pessoa {
+public class Jogador extends Pessoa{
 
-	// =======================================================
-	// ===================== ATRIBUTOS =======================
-	// =======================================================
+	 // =======================================================
+    // ===================== ATRIBUTOS =======================
+    // =======================================================
 
+	
 	private String posicao;
 	private String pe;
 
-	// =======================================================
-	// ============ ACESSORES e MODIFICADORES ===============
-	// =======================================================
+    // =======================================================
+    // ============ ACESSORES e MODIFICADORES ===============
+    // =======================================================
 	public String getPosicao() {
 		return posicao;
 	}
-
 	public void setPosicao(String posicao) {
 		this.posicao = posicao;
 	}
-
 	public String getPe() {
 		return pe;
 	}
-
 	public void setPe(String pe) {
 		this.pe = pe;
 	}
 
-	// =======================================================
-	// =================== CONSTRUTORES =====================
-	// =======================================================
 
-	// CONSTRUTOR DEFAULT
+    // =======================================================
+    // =================== CONSTRUTORES =====================
+    // =======================================================
 
-	// CONSTRUTOR COM PAR�METROS
+  
+   
+    // CONSTRUTOR DEFAULT
 
-	// =======================================================
-	// =================== .COMPORTAMENTOS ==================
-	// =======================================================
+   
 
-public String mostrarTudo() {
-        String mostrar = "";
-        mostrar +=  "======JOGADOR======" + "\n";
+    // CONSTRUTOR COM PARÂMETROS
 
-        mostrar += "Nome: " + this.getNome() + "\n";
-        mostrar += "Contato: " + this.getContacto() + "\n";
-        mostrar += "Email: " + this.getEmail() + "\n";
-        mostrar += "Cartão de cidadão: " + this.getNumeroCartaoCidadao() + "\n";
-        mostrar += "Pé preferido: " + this.getPe() + "\n";
-        mostrar += "Posição: " + this.getPosicao() + "\n" ;
-        mostrar += "\n" ;
 
-        return mostrar;
-    }
 
-	// =======================================================
-	// ============== M�TODOS COMPLEMENTARES ==============
-	// =======================================================
+    // =======================================================
+    // =================== .COMPORTAMENTOS ==================
+    // =======================================================
 
-	// ----> toString()
+	public String mostrarTudo() {
+		String mostrar = "";
+		mostrar +=  "======JOGADOR======" + "\n";
 
-	// ----> equals()
+		mostrar += "Nome: " + this.getNome() + "\n";
+		mostrar += "Contato: " + this.getContacto() + "\n";
+		mostrar += "Email: " + this.getEmail() + "\n";
+		mostrar += "Cartão de cidadão: " + this.getNumeroCartaoCidadao() + "\n";
+		mostrar += "Pé preferido: " + this.getPe() + "\n";
+		mostrar += "Posição: " + this.getPosicao() + "\n" ;
+		mostrar += "\n" ;
+
+		return mostrar;
+	}
+
+
+    // =======================================================
+    // ============== MÉTODOS COMPLEMENTARES ==============
+    // =======================================================
+
+    // ----> toString()
+
+	@Override
+	public String toString() {
+		return "Jogador [posicao=" + posicao + ", pe=" + pe + "]";
+	}
+
+    // ----> equals()
 }
